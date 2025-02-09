608eaac1d47fbe2859aa2ffec60e6d2504faf5f4
Felipe <felipe.rodrigues@sebsa.com.br>
2025-02-09 10:14:41 -0300

    feat: add domain with student entity

---
+++ src/SagaExampleMassTransit.Domain/Entities/Student.cs
@@ -0,0 +1,34 @@
+﻿namespace SagaExampleMassTransit.Domain.Entities
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public Guid UId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public Guid? ThirdPartyStudentUId { get; set; }
+    }
+
+    public static class StudentFactory
+    {
+        public static Student CreateStudent(string firstName, string lastName, DateTime birthDate, string email)
+        {
+            return new Student
+            {
+                UId = Guid.NewGuid(),
+                FirstName = firstName,
+                LastName = lastName,
+                BirthDate = birthDate,
+                Email = email
+            };
+        }
+
+        public static Student UpdateThirPartyStudentUId(Student student, Guid thirdPartyStudentUId)
+        {
+            student.ThirdPartyStudentUId = thirdPartyStudentUId;
+            return student;
+        }
+    }
+}
