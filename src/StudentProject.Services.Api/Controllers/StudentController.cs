5c9a3f2c18766548208ccbecc2165e8871781613
Felipe <felipe.rodrigues@sebsa.com.br>
2025-02-15 10:23:19 -0300

    feat: alterar o local de envio dos eventos para a camada de serviço, pois o handler pode ser usado por outras partes

--- src/StudentProject.Services.Api/Controllers/StudentController.cs
+++ src/StudentProject.Services.Api/Controllers/StudentController.cs
@@ -1,8 +1,10 @@
 ﻿using Asp.Versioning;
+using MassTransit;
 using MassTransit.Mediator;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using StudentProject.Contracts;
 using StudentProject.Domain.Mediator;
 using StudentProject.Domain.Mediator.Notifications;
 using StudentProject.Domain.Students.Commands;
@@ -16,13 +18,16 @@ namespace StudentProject.Services.Api.Controllers
     public class StudentController : ApiController
     {
         private readonly IMediatorHandler _mediator;
+        private readonly IBus _bus;
 
         public StudentController(
             ILogger<StudentController> logger,
             INotificationHandler<DomainNotification> notifications,
-            IMediatorHandler mediator) : base(logger, notifications)
+            IMediatorHandler mediator,
+            IBus bus) : base(logger, notifications)
         {
             _mediator = mediator;
+            _bus = bus;
         }
 
         [AllowAnonymous]
@@ -45,7 +50,50 @@ namespace StudentProject.Services.Api.Controllers
         [HttpPost("student/create")]
         public async Task<IActionResult> CreateStudentAsync([FromBody] CreateStudentCommand command, CancellationToken cancellationToken)
         {
-            await _mediator.SendCommand(command, cancellationToken);
+            if (command == null)
+                await _notifications.Handle(new DomainNotification("request", "The request body cannot be null"), cancellationToken);
+
+            try
+            {
+                await _mediator.SendCommand(command, cancellationToken);
+
+                if (!_notifications.HasNotifications())
+                {
+                    await _bus.Publish<StudentCreated>(new StudentCreated
+                    {
+                        UId = command.UId,
+                        FirstName = command.FirstName,
+                        LastName = command.LastName,
+                        BirthDate = command.BirthDate,
+                        Email = command.Email
+                    });
+                }
+                else
+                {
+                    await _bus.Publish<CreateStudentValidationFailed>(new CreateStudentValidationFailed
+                    {
+                        FirstName = command.FirstName,
+                        LastName = command.LastName,
+                        BirthDate = command.BirthDate,
+                        Email = command.Email,
+                        ValidationErrors = _notifications.GetNotifications().ToDictionary(a => a.Key, b => b.Value)
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                await _bus.Publish<CreateStudentFailed>(new CreateStudentFailed
+                {
+                    FirstName = command.FirstName,
+                    LastName = command.LastName,
+                    BirthDate = command.BirthDate,
+                    Email = command.Email,
+
+                    ExceptionMessage = e.Message.ToString(),
+                    ExceptionStackTrace = e.StackTrace,
+                    ExceptionType = e.GetType().ToString(),
+                });
+            }
             return ResponseApi(command);
         }
 
